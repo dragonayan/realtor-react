4044f8bfefbdb99da82b86f3756232db0aa0a1b3
dragonayan <ayan.cse120067@bppimt.ac.in>
2023-07-23 22:19:31 +0530

    spinner.jsx change

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -6,6 +6,7 @@ export default function Spinner() {
     <div className='bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 top-0 bottom-0 z-60' >
         <div>
             <img src={spinner} alt="loading.." className='h-24' />
+            ok this is the spinner
         </div>
     </div>
   )
